2a9793a7cc39bfb1e90ffb9ae71b798b00ff8bd0
Connor E. Haight <connor.haight@gmail.com>
2016-07-14 20:32:26 +0000

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,11 +1,20 @@
+# Define Variable Object
+class Variable(object):
+  def __init__(self, key, value):
+    self.key = key
+    self.value = value
+
+# Define String Object
 class String(object):
   def __init__(self, value):
     self.value = value
 
+# Define Args Object
 class Args(object):
   def __init__(self, value):
     self.value = value
-    
+
+# Define List Object
 class List(object):
   def __init__(self, value):
     self.value = value
