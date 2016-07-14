84d4b5d6acb4f59eefdffecd0c2aa91f46fa7993
Connor E. Haight <connor.haight@gmail.com>
2016-07-14 20:36:47 +0000

    Update essl.py

--- essl.py
+++ essl.py
@@ -8,23 +8,6 @@ import classes
 functions = []
 variables = []
 
-def recurse(source):
-    compiledScript = ''
-    if source[0] == 'args' and not source[1] in ('+', '-', '*', '/'):
-        for arg in source[2:]:
-            if isinstance(arg, list):
-                compiledScript += recurse(arg) + '\tpush eax\n'
-            else:
-                compiledScript += '\tpush ' + getVar(arg) + '\n'
-        compiledScript += '\tcall ' + source[1] + '\n'
-    else:
-        if isinstance(getVar(source), list):
-            if getVar(source)[0] == 'math':
-                compiledScript += getVar(source)[1]
-        #else:
-        #    compiledScript += getVar(source)# + '\n'
-    return compiledScript
-        
 # Compile the script into assembly
 def turingCompile(function):
     compiledScript = ''
