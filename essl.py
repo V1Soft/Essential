8c290c7186cdad2d3b302f5b1ffa83718bfc8e96
Connor E. Haight <connor.haight@gmail.com>
2016-07-14 20:35:52 +0000

    Update essl.py

--- essl.py
+++ essl.py
@@ -8,32 +8,6 @@ import classes
 functions = []
 variables = []
 
-# Defining all variables
-def lex(parsedScript):
-    lexedScript = parsedScript
-    index = 0
-    for structure in parsedScript:
-        if structure[0] == 'use':
-            proto = ''
-            pkg = ''
-            if structure[1].startswith('.'):
-                proto = structure[1][1:]
-            else:
-                proto = '/' + structure[1]
-            for char in proto:
-                if char == '.':
-                    pkg += '/'
-                elif char == '/':
-                    print('Error: Invalid Syntax.')
-                    sys.exit(1)
-                else:
-                    pkg += char
-            lex(parse(open(pkg + '.essl', 'r+').read()))
-        elif structure[0][0] == 'subroutine':
-            functions.append(Variable(structure[0][1], structure[1:]))
-            lexedScript[index].remove(structure[0])
-        index += 1
-
 def recurse(source):
     compiledScript = ''
     if source[0] == 'args' and not source[1] in ('+', '-', '*', '/'):
