905b76fe9763bca9a1ee7e44f21aae68c43bccb1
Connor E. Haight <connor.haight@gmail.com>
2016-07-14 20:28:44 +0000

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,1 +1,11 @@
 class String(object):
+  def __init__(self, value):
+    self.value = value
+
+class Args(object):
+  def __init__(self, value):
+    self.value = value
+    
+class List(object):
+  def __init__(self, value):
+    self.value = value
